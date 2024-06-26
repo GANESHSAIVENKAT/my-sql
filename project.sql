a5258f794fd3e950ebf5e43015008f53abc34bdd
GANESHSAIVENKAT <ganesh043.xworkz@gmail.com>
2024-06-26 14:30:05 +0530

    project(26-06-2024)

---
+++ project.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE project;
+CREATE TABLE Airport_details(id int, airport_name varchar(20), arrival_Time int,departure_Time int ,price_in_Rs int);
+
+CREATE TABLE Cricket_teams(id int, team_name varchar(20),number_of_players int, captain_name varchar(20), coach_name varchar(20));
